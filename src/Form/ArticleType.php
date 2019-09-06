b53fb58c098f67523288deaa1b81f3db3a6aa8fe
Mamadou Aliou Diallo <alioukahere@gmail.com>
2019-09-06 00:44:33 +0000

    Adding ArticleType

---
+++ src/Form/ArticleType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use App\Entity\Category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('picture', FileType::class)
+            ->add('title', TextType::class)
+            ->add('content', TextareaType::class)
+            ->add('isPublished', CheckboxType::class)
+            ->add('categories', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'label',
+                'multiple' => true,
+                'expanded' => false
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
